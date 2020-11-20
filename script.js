22c7b6f158392a235ce57ff88366884f22284348
aamree <adilet90@mail.ru>
2020-11-20 19:56:21 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+
+// Показать спинер
+function showLoadingSpinner() {
+  loader.hidden = false
+  quoteContainer.hidden = true
+}
+
+// Скрыть спинер
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false
+    loader.hidden = true
+  }
+}
+
+// Получение цитаты из API
+async function getQuote() {
+  showLoadingSpinner();
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+  const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=ru&format=json'
+
+  try {
+    const response = await fetch(proxyUrl +  apiUrl)
+    const data  = await response.json()
+    // Если автор цитаты неизвестно
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Неизвестный автор'
+    } else {
+      authorText.innerText = data.quoteAuthor
+    }
+    // Для длинных цитат
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote')
+    } else {
+      quoteText.classList.remove('long-quote')
+    }
+    quoteText.innerText = data.quoteText
+    // Остановить спинер и показать цитату
+    removeLoadingSpinner()
+  } catch (error) {
+    // TODO: доработать обработку ошибок. Данный код потенциально может привезти к вечному циклу. 
+    getQuote()
+  }
+}
+
+// Твитнуть цитату 
+function tweetQuote() {
+  const quote = quoteText.innerText
+  const author = authorText.innerText
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`
+
+  window.open(twitterUrl, '_blank')
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote)
+twitterBtn.addEventListener('click', tweetQuote)
+
+// On Load
+getQuote()
